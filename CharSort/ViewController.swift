be319d9274f40ac129a291811ecc933ff27c8805
Wasith Theerapattrathamrong <phaicpe@gmail.com>
2016-07-30 13:47:57 +0700

    Write automate test withui

--- CharSort/ViewController.swift
+++ CharSort/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputTextField: UITextField!
+    @IBOutlet weak var outputLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func submitButtonClicked(sender: AnyObject) {
+        let sorted = Sorter().sort(inputTextField.text!)
+        outputLabel.text = sorted
+    }
 
 }
-
---
+++ CharSortUITests/CharSortUITests.swift
@@ -0,0 +1,42 @@
+//
+//  CharSortUITests.swift
+//  CharSortUITests
+//
+//  Created by Wasith Theerapattrathamrong on 7/30/2559 BE.
+//  Copyright © 2559 Wasith Theerapattrathamrong. All rights reserved.
+//
+
+import XCTest
+
+class CharSortUITests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func test_displaySortedCharWhenLickedSort() {
+        let app = XCUIApplication()
+
+        app.textFields["inputTextField"].tap()
+        app.textFields["inputTextField"].typeText("Dog")
+
+        app.buttons["submitButton"].tap()
+
+        XCTAssertEqual("dgo", app.staticTexts["outputLabel"].label)
+    }
+
+}
