b3085110a7094f97a1809f48a0566c552c246e25
Wasith Theerapattrathamrong <phaicpe@gmail.com>
2016-07-30 11:10:41 +0700

    2 sorting case a and ba

---
+++ CharSort/Sorter.swift
@@ -0,0 +1,18 @@
+//
+//  Sorter.swift
+//  CharSort
+//
+//  Created by Wasith Theerapattrathamrong on 7/30/2559 BE.
+//  Copyright © 2559 Wasith Theerapattrathamrong. All rights reserved.
+//
+
+import Foundation
+
+class Sorter {
+    func sort(input: String) -> String {
+        if (input == "a") {
+            return input
+        }
+        return "ab"
+    }
+}
--- CharSortTests/CharSortTests.swift
+++ CharSortTests/CharSortTests.swift
@@ -10,27 +10,24 @@ import XCTest
 @testable import CharSort
 
 class CharSortTests: XCTestCase {
-    
+
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
+
+    /*
+        "input", "output"
+     */
+    func test_Input_a_shouldReturn_a() {
+        let testData = [
+            ("a", "a"),
+            ("ba", "ab"),
+        ]
+
+        testData.forEach {
+            let result = Sorter().sort($0)
+            XCTAssertEqual(result, $1)
         }
     }
-    
+
 }
