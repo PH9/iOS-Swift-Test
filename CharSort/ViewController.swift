d40d36fffca4cc47e027a0c14ac9d2169681283a
Wasith Theerapattrathamrong <phaicpe@gmail.com>
2016-07-30 13:51:16 +0700

    Remove un override method

--- CharSort/ViewController.swift
+++ CharSort/ViewController.swift
@@ -13,16 +13,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var inputTextField: UITextField!
     @IBOutlet weak var outputLabel: UILabel!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     @IBAction func submitButtonClicked(sender: AnyObject) {
         let sorted = Sorter().sort(inputTextField.text!)
         outputLabel.text = sorted
