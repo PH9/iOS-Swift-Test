13684a635384d02d1594fb0979cc9ab1c3a11650
Wasith Theerapattrathamrong <phaicpe@gmail.com>
2016-07-30 15:20:45 +0700

    UI Test when enter nothing clear show result

--- CharSortUITests/CharSortUITests.swift
+++ CharSortUITests/CharSortUITests.swift
@@ -28,7 +28,7 @@ class CharSortUITests: XCTestCase {
         super.tearDown()
     }
 
-    func test_displaySortedCharWhenLickedSort() {
+    func test_displaySortedChar_WhenLickedSort() {
         let app = XCUIApplication()
 
         app.textFields["inputTextField"].tap()
@@ -37,6 +37,27 @@ class CharSortUITests: XCTestCase {
         app.buttons["submitButton"].tap()
 
         XCTAssertEqual("dgo", app.staticTexts["outputLabel"].label)
+    }
+
+    func test_displayEmpty_whenEnterEmpty() {
+        let app = XCUIApplication()
+
+        app.textFields["inputTextField"].tap()
+        app.textFields["inputTextField"].typeText("Dog")
+
+        app.buttons["submitButton"].tap()
+
+        XCTAssertEqual("dgo", app.staticTexts["outputLabel"].label)
+
+        app.textFields["inputTextField"].tap()
+        let deleteKey = XCUIApplication().keys["delete"]
+        deleteKey.tap()
+        deleteKey.tap()
+        deleteKey.tap()
+
+        app.buttons["submitButton"].tap()
+
+        XCTAssertEqual("", app.staticTexts["outputLabel"].label)
     }
 
 }
